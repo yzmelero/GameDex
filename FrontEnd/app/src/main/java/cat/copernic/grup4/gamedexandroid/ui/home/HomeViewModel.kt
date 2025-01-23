4a000fbb76301a81cc921d84d7e77bd46e4ba833
Anna <aserrano@alumnat.copernic.cat>
2025-01-23 10:53:03 +0100

    Primera pujada amb index.adoc i el projecte d'Android creat.

---
+++ FrontEnd/app/src/main/java/cat/copernic/grup4/gamedexandroid/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package cat.copernic.grup4.gamedexandroid.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
---
+++ FrontEnd/app/src/main/java/cat/copernic/grup4/gamedexandroid/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package cat.copernic.grup4.gamedexandroid.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
