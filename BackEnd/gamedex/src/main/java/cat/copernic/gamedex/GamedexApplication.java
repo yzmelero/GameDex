de82f518b13ab7c3ec2c3ae3b9f70c1a50dd6b6b
Anna <aserrano@alumnat.copernic.cat>
2025-01-31 13:03:12 +0100

    Afegit projecte al BackEnd.

---
+++ BackEnd/gamedex/src/main/java/cat/copernic/gamedex/GamedexApplication.java
@@ -0,0 +1,13 @@
+package cat.copernic.gamedex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamedexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamedexApplication.class, args);
+	}
+
+}
