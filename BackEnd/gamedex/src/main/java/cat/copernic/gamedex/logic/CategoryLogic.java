c593c72bf0d6e8e458560844cd78231d78a2b01f
Héctor Vico García <vico.garcia.hector@alumnat.copernic.cat>
2025-02-06 08:36:43 +0100

    Añadir Categoria esta hecho falta comprobar y ahra voy a hacer un commit para hacer pull y que se me guarde lo de los compañeros.

--- BackEnd/gamedex/src/main/java/cat/copernic/gamedex/logic/CategoryLogic.java
+++ BackEnd/gamedex/src/main/java/cat/copernic/gamedex/logic/CategoryLogic.java
@@ -1,5 +1,18 @@
 package cat.copernic.gamedex.logic;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import cat.copernic.gamedex.entity.Category;
+import cat.copernic.gamedex.repository.CategoryRepository;
+
+@Service
 public class CategoryLogic {
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    public Category createCategory(Category category) {
+        return categoryRepository.save(category);
+    }
 }
