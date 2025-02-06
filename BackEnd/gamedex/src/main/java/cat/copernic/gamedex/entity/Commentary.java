a1bb104455f7d38281da35a9af8d684617674383
Anna <aserrano@alumnat.copernic.cat>
2025-02-06 09:50:42 +0100

    Funcionalitat afegirComentari acabada, falta la pantalla.

--- BackEnd/gamedex/src/main/java/cat/copernic/gamedex/entity/Commentary.java
+++ BackEnd/gamedex/src/main/java/cat/copernic/gamedex/entity/Commentary.java
@@ -1,15 +1,11 @@
-
 package cat.copernic.gamedex.entity;
 
-import cat.copernic.gamedex.entity.User;
-import cat.copernic.gamedex.entity.Videogame;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import lombok.Data;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
-import java.util.List;
 
 @Document(collection = "commentary")
 @Data
@@ -30,6 +26,6 @@ public class Commentary {
     private User user;
 
     @DBRef
-    private List<Videogame> videogame;
+    private Videogame videogame;
 
     }
--- BackEnd/gamedex/src/main/java/cat/copernic/gamedex/repository/CommentaryRepository.java
+++ BackEnd/gamedex/src/main/java/cat/copernic/gamedex/repository/CommentaryRepository.java
@@ -1,10 +1,7 @@
-
-
 package cat.copernic.gamedex.repository;
 
-import cat.copernic.gamedex.model.Commentary;
+import cat.copernic.gamedex.entity.Commentary;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
 
 public interface CommentaryRepository extends MongoRepository<Commentary, String>{
 
