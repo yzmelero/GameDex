70d10340b7f64e6b39d520b6d4d008e7e3dcbf7d
Héctor Vico García <vico.garcia.hector@alumnat.copernic.cat>
2025-02-10 12:13:06 +0100

    Añadido la entidad

--- FrontEnd/app/src/main/java/cat/copernic/grup4/gamedex/Core/Model/Category.kt
+++ FrontEnd/app/src/main/java/cat/copernic/grup4/gamedex/Core/Model/Category.kt
@@ -1,20 +1,8 @@
 package cat.copernic.grup4.gamedexandroid.Core.Model
 
-import java.time.LocalDate
+data class Category(
+    val nameCategory: String,
+    val description: String,
+    val categoryPhoto: String,
 
-data class User(
-    val username: String,
-    val password: String,
-    val name: String,
-    val surname: String,
-    val email: String,
-    val telephone: Int,
-
-    val birthDate: LocalDate,
-
-    val profilePicture: ByteArray, // MongoDB almacena Binary[], en Kotlin usamos ByteArray
-
-    val state: Boolean, // 0 (false) para deshabilitado, 1 (true) para validado
-
-    val userType: UserType
 )
