e02b420288fd1371817f12517d05f5bd59ba7c85
Anna <aserrano@alumnat.copernic.cat>
2025-02-14 11:40:35 +0100

    Afegit mètode login a l'UserApiRest i creades les classes necessàries pel login.

--- FrontEnd/app/src/main/java/cat/copernic/grup4/gamedex/Users/Data/UserApiRest.kt
+++ FrontEnd/app/src/main/java/cat/copernic/grup4/gamedex/Users/Data/UserApiRest.kt
@@ -1,5 +1,7 @@
 package cat.copernic.grup4.gamedex.Users.Data
 
+import cat.copernic.grup4.gamedex.Core.Model.LoginRequest
+import cat.copernic.grup4.gamedex.Core.Model.LoginResponse
 import cat.copernic.grup4.gamedex.Core.Model.User
 import retrofit2.Call
 import retrofit2.Response
@@ -19,6 +21,11 @@ interface UserApiRest {
 
     @GET("user/all")
     suspend fun getAllUsers(): Response<List<User>>
+
+    @POST("login/verify")  // Endpoint que has definit al backend
+    fun loginUser(@Body request: LoginRequest): Call<LoginResponse>
+
+
 /*
     @GET("user/byId/{userId}")
     suspend fun getUserById(@Path("userId") userId: String): Response<User>
