d1fb2fc2f59b80272e2f8e9cc34912c5ea90861b
Anna <aserrano@alumnat.copernic.cat>
2025-02-14 09:19:54 +0100

    Afegit el mètode findByUsername al Repositori de User

--- BackEnd/gamedex/src/main/java/cat/copernic/gamedex/repository/UserRepository.java
+++ BackEnd/gamedex/src/main/java/cat/copernic/gamedex/repository/UserRepository.java
@@ -13,6 +13,7 @@ public interface UserRepository extends MongoRepository<User, String> {
 
     List<User> findByUsernameContaining(String username);
 
+    Optional<User> findByUsername(String username);
     Optional<User> findByEmail(String email);
 
     Optional<User> findByTelephone(int telephone);
