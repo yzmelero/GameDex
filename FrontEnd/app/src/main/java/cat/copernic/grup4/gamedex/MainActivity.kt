6a296c4ecc5980efee348ec2db8e0b2af2ddb94f
Mario Olaya Perez <molaya@alumnat.copernic.cat>
2025-02-18 09:46:23 +0000

    Edit MainActivity.kt

--- FrontEnd/app/src/main/java/cat/copernic/grup4/gamedex/MainActivity.kt
+++ FrontEnd/app/src/main/java/cat/copernic/grup4/gamedex/MainActivity.kt
@@ -13,8 +13,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContent {
-            //AppNavigation()
-            UserListScreen()
+            AppNavigation()
         }
     }
 }
