bbf6117c292d1622d4138c14430c6ab3aab18f96
Héctor Vico García <vico.garcia.hector@alumnat.copernic.cat>
2025-02-19 10:23:44 +0100

    He modificado el Main, sin querer no lo he dejado como estaba

--- FrontEnd/app/src/main/java/cat/copernic/grup4/gamedex/MainActivity.kt
+++ FrontEnd/app/src/main/java/cat/copernic/grup4/gamedex/MainActivity.kt
@@ -4,8 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.navigation.compose.rememberNavController
-import cat.copernic.grup4.gamedex.Category.UI.Screens.AddCategoryScreen
 import cat.copernic.grup4.gamedex.Core.ui.navigation.AppNavigation
 
 class MainActivity : ComponentActivity() {
@@ -13,8 +11,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContent {
-            val fakeNavController = rememberNavController()
-            AddCategoryScreen(navController = fakeNavController)
+            AppNavigation()
         }
     }
 }
