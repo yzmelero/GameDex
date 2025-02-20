5aab814cd5479bff4f2b096cac1411823b2651c1
YaizaMelero <ymelero@alumnat.copernic.cat>
2025-02-20 14:18:09 +0100

    Afegint TO-DO a les pantalles

--- FrontEnd/app/src/main/java/cat/copernic/grup4/gamedex/videogames/ui/screens/AddGames.kt
+++ FrontEnd/app/src/main/java/cat/copernic/grup4/gamedex/videogames/ui/screens/AddGames.kt
@@ -46,7 +46,6 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -256,6 +255,7 @@ fun AddGameFormFields(
         )
         Spacer(modifier = Modifier.height(20.dp))
         TextField(
+            // TODO desplegable de categoria
             value = nameCategory,
             onValueChange = onCategoryChange,
             label = {
@@ -289,6 +289,7 @@ fun textFieldModifier() = Modifier
 @Composable
 fun ImagePicker() {
     Row {
+        // TODO Afegir imatge
         Text(
             text = stringResource(R.string.cover) + ":",
             color = Color.Black,
