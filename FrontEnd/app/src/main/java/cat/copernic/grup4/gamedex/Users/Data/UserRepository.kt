d301f52868bf371e1e5d0b6bdc848f6984e30342
Anna <aserrano@alumnat.copernic.cat>
2025-02-20 08:14:35 +0100

    Arreglada la funció loginUser del repositori de User.

--- FrontEnd/app/src/main/java/cat/copernic/grup4/gamedex/Users/Data/UserRepository.kt
+++ FrontEnd/app/src/main/java/cat/copernic/grup4/gamedex/Users/Data/UserRepository.kt
@@ -3,6 +3,7 @@ package cat.copernic.grup4.gamedex.Users.Data
 import cat.copernic.grup4.gamedex.Core.Model.LoginRequest
 import cat.copernic.grup4.gamedex.Core.Model.LoginResponse
 import cat.copernic.grup4.gamedex.Core.Model.User
+import cat.copernic.grup4.gamedex.Users.Data.RetrofitInstance.api
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -12,19 +13,8 @@ class UserRepository {
         return RetrofitInstance.api.registerUser(user)
     }
 
-    fun loginUser(loginRequest: LoginRequest, callback: (Result<LoginResponse>) -> Unit) {
-        RetrofitInstance.api.loginUser(loginRequest).enqueue(object : Callback<LoginResponse> {
-            override fun onResponse(call: Call<LoginResponse>, response: Response<LoginResponse>) {
-                if (response.isSuccessful) {
-                    response.body()?.let { callback(Result.success(it)) }
-                } else {
-                    callback(Result.failure(Exception("Error: ${response.message()}")))
-                }
-                }
-
-            override fun onFailure(call: Call<LoginResponse>, t: Throwable) {
-                callback(Result.failure(t))
-            }
-        })
+    suspend fun loginUser(username: String, password: String): Response<LoginResponse> {
+        val request = LoginRequest(username, password)  //
+        return RetrofitInstance.api.loginUser(request)
     }
 }
