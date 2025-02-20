998d45f25b10cec0655cc193c022964535617748
Anna <aserrano@alumnat.copernic.cat>
2025-02-20 08:12:01 +0100

    He esborrat el fitxer de UseCaseLogin i he afegit el seu contingut

--- FrontEnd/app/src/main/java/cat/copernic/grup4/gamedex/Users/Domain/UseCaseSignUp.kt
+++ FrontEnd/app/src/main/java/cat/copernic/grup4/gamedex/Users/Domain/UseCaseSignUp.kt
@@ -7,5 +7,8 @@ import cat.copernic.grup4.gamedex.Core.Model.User
 
 class UseCases(private val repository: UserRepository) {
     suspend fun registerUser(user: User) = repository.registerUser(user)
+
+    suspend fun loginUser(username: String, password: String) = repository.loginUser(username, password)
+
 }
 
