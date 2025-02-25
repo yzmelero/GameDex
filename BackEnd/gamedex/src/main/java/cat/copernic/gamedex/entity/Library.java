b94e88ffc4790017397a24a6effdf377e5f88ac8
Anna <aserrano@alumnat.copernic.cat>
2025-02-25 09:12:48 +0100

    Canviat commentary per library

---
+++ BackEnd/gamedex/src/main/java/cat/copernic/gamedex/entity/Library.java
@@ -0,0 +1,36 @@
+package cat.copernic.gamedex.entity;
+
+import java.time.LocalDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+@Document(collection = "library")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Library {
+
+    @Id
+    @Field(name = "idlibrary")
+    private String idLibrary;
+
+    private StateType state;
+
+    private String description;
+
+    private double rating;
+
+    @DBRef
+    private User user;
+
+    @DBRef
+    private Videogame videogame;
+
+    @Field(name = "published_date")
+    private LocalDate publishedDate;
+    }
