c5ec750a0d950cacb690adb337bc1cde9b8311f0
Anna <aserrano@alumnat.copernic.cat>
2025-02-27 08:33:37 +0100

    Afegida una String al fitxer resources per a AddCategory

--- FrontEnd/app/src/main/java/cat/copernic/grup4/gamedex/Category/UI/Screens/AddCategory.kt
+++ FrontEnd/app/src/main/java/cat/copernic/grup4/gamedex/Category/UI/Screens/AddCategory.kt
@@ -179,6 +179,7 @@ fun TextField(label: String, text: String, height: Dp = 80.dp, onTextChanged: (S
 
 @Composable
 fun ImageUploadSection(imageUri: Uri?, onImageClick: () -> Unit) {
+    val context = LocalContext.current
     Box(
         modifier = Modifier
             .fillMaxWidth()
@@ -192,7 +193,7 @@ fun ImageUploadSection(imageUri: Uri?, onImageClick: () -> Unit) {
         }
         Image(
             painter = painter,
-            contentDescription = "Image select",
+            contentDescription = context.getString(R.string.imageSelect),
             modifier = Modifier.fillMaxSize()
         )
         IconButton(
