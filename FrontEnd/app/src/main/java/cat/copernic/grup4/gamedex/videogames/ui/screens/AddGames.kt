34c045ec14498d6d4f6477b1bb717bc0683a1d4a
YaizaMelero <ymelero@alumnat.copernic.cat>
2025-03-03 11:12:49 +0100

    Arreglat una cosa d'addgames

--- FrontEnd/app/src/main/java/cat/copernic/grup4/gamedex/videogames/ui/screens/AddGames.kt
+++ FrontEnd/app/src/main/java/cat/copernic/grup4/gamedex/videogames/ui/screens/AddGames.kt
@@ -173,7 +173,6 @@ fun AddGamesScreen(navController : NavController, userViewModel: UserViewModel) 
                             color = Color.Black,
                             //style = GameDexTypography.bodyLarge,
                             fontSize = 18.sp,
-
                             modifier = Modifier.padding(end = 100.dp, bottom = 4.dp)
                         )
 
